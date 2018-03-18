bbe318a84f8147ef5968c620cbe2bdf79307dad7
seiki tsunoda <liquidohr@gmail.com>
2018-03-18 11:20:38 -0700

    data moved to seed.js

---
+++ animated-carousel/app/js/seed.js
@@ -0,0 +1,52 @@
+const positions = [
+  {
+    id: 0,
+    textTop: '50%',
+    imgTop: '50%',
+    textLeft: '31%',
+    imgLeft: '74%',
+    url: 'https://res.cloudinary.com/liquidohr/image/upload/v1521308653/sample1.jpg',
+    background: 'linear-gradient(-135deg, #ad0103 20%, #6d0204)',
+    imgBackground: '#fc5617'
+  },
+  {
+    id: 1,
+    textTop: '60%',
+    imgTop: '50%',
+    textLeft: '70%',
+    imgLeft: '24%',
+    url: 'https://res.cloudinary.com/liquidohr/image/upload/v1521316854/sample2.jpg',
+    background: 'linear-gradient(-135deg, #1e6161 20%, #081b1b)',
+    imgBackground: '#2d9191'
+  },
+  {
+    id: 2,
+    textTop: '45%',
+    imgTop: '52%',
+    textLeft: '70%',
+    imgLeft: '26%',
+    url: 'https://res.cloudinary.com/liquidohr/image/upload/v1521322158/sample3.jpg',
+    background: 'linear-gradient(-135deg, #feb301 20%, #644602)',
+    imgBackground: '#ffb206'
+  },
+  {
+    id: 3,
+    textTop: '55%',
+    imgTop: '48%',
+    textLeft: '32%',
+    imgLeft: '74%',
+    url: 'https://res.cloudinary.com/liquidohr/image/upload/v1521322417/sample4.jpg',
+    background: 'linear-gradient(-135deg, #1b7100 20%, #104400)',
+    imgBackground: '#19a500'
+  },
+  {
+    id: 4,
+    textTop: '40%',
+    imgTop: '48%',
+    textLeft: '69%',
+    imgLeft: '23%',
+    url: 'https://res.cloudinary.com/liquidohr/image/upload/v1521322952/sample5.jpg',
+    background: 'linear-gradient(-135deg, #346bac 20%, #0d2e59)',
+    imgBackground: '#428fe5'
+  }
+]
