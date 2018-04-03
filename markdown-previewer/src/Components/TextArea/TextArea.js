97a3557c22a70a2563160c571606531c81c6f7a1
seiki tsunoda <liquidohr@gmail.com>
2018-04-02 19:06:55 -0700

    basic function done

--- markdown-previewer/src/Components/TextArea/TextArea.js
+++ markdown-previewer/src/Components/TextArea/TextArea.js
@@ -8,13 +8,19 @@ class TextArea extends Component {
         value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n'
     }
 
-    handleChange = () => {
-        this.setState({value: this.refs.textarea.value});
+    textChangeHandler = () => {
+        this.setState(
+            {
+                value: this.refs.textarea.value
+            }
+        );
       }
       
-    createMarkup = () => {
+    markupHandler = () => {
         let markdown = marked(this.state.value);
-        return {__html: markdown}
+        return {
+            __html: markdown
+        }
     }
 
     render() {
@@ -24,13 +30,13 @@ class TextArea extends Component {
                 <div className={classes.TextArea}>
                     <textarea
                         className={classes.TextField}
-                        onChange={this.handleChange}
+                        onChange={this.textChangeHandler}
                         ref="textarea"
                         defaultValue={value}></textarea>
                 </div>
                 <div 
                     className={classes.Output} 
-                    dangerouslySetInnerHTML={this.createMarkup()}></div>
+                    dangerouslySetInnerHTML={this.markupHandler()}></div>
             </div>
         )
     }
